0f2a26f4f5373f8a046ad0ba0fb671fdc9246ca3
bradblan <32416731+bradblan@users.noreply.github.com>
2020-10-15 23:13:55 -0400

    Add path to markdown content into gatsby config filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
